9e612f437f7dbf942da3f578eccb5e57ae514efc
BarrettPalmer <barrettapalmer@gmail.com>
2024-07-30 22:34:13 -0700

    Create page.vue

---
+++ pages/page.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- Display the "time ago" result using the timeAgoFunction -->
+    <div>{{timeAgoFunction({ _seconds: 1722397001, _nanoseconds: 975000000 })}}</div>
+  </div>
+</template>
+<script setup lang="ts">
+import { timeAgo } from "~/composables/timeAgoHelper";// Import the timeAgo function from the helper
+
+const timeAgoFunction = (timestamp: String) => {
+  try {
+    // Call the timeAgo helper function with the provided timestamp
+    return timeAgo(timestamp);
+  } catch (error) {
+    // Log any errors to the console and return "Invalid date"
+    console.error(error);
+    return "Invalid date";
+  }
+};
+</script>
